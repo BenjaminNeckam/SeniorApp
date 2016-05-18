babf0d7a39d154132d5f98a881385b1594c0c7ae
Benjamin Neckam <a1301917@unet.univie.ac.at>
2016-05-18 15:22:39 +0200

    Kleine Änderung bezüglivh funktionalität

--- app/src/main/java/a1301917/at/ac/univie/hci/seniorapp/MenueActivity.java
+++ app/src/main/java/a1301917/at/ac/univie/hci/seniorapp/MenueActivity.java
@@ -1,15 +1,10 @@
 package a1301917.at.ac.univie.hci.seniorapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.SimpleAdapter;
-import android.widget.Toast;
 
-//// TODO: 16.05.2016 Größe der Grids so ändern das sie ganzes layout füllen
 public class MenueActivity extends AppCompatActivity {
     private String[] menue;
     @Override
@@ -18,17 +13,16 @@ public class MenueActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menue);
 
         menue = getResources().getStringArray(R.array.menue_button_names);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, menue);
-        GridView gridView = (GridView) findViewById(R.id.MenuGrid);
-        gridView.setAdapter(adapter);
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MenueActivity.this, "" + position,
-                        Toast.LENGTH_SHORT).show();
 
-            }
-        });
+    }
+
+    public void MenueOn(View view){
+        Intent intent = new Intent(this, MenueActivity.class);
+        startActivity(intent);
+    }
+
+    public void MenueOff(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
