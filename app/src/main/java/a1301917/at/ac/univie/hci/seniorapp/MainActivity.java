d0a527f77fa9c82c27e62bbf04adc19dc1e50dd7
Benjamin Neckam <a1301917@unet.univie.ac.at>
2016-05-20 09:48:13 +0200

    Weitere Veränderungen beim Layout

--- app/src/main/java/a1301917/at/ac/univie/hci/seniorapp/MainActivity.java
+++ app/src/main/java/a1301917/at/ac/univie/hci/seniorapp/MainActivity.java
@@ -1,8 +1,8 @@
 package a1301917.at.ac.univie.hci.seniorapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,13 +13,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void MenueOn(View view){
-        Intent intent = new Intent(this, MenueActivity.class);
+    public void MenuOn(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
     }
 
-    //// TODO: 16.05.2016 Wirklich notwendig? 
-    public void MenueOff(View view){
+    public void MenuOff(View view){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
---
+++ app/src/main/java/a1301917/at/ac/univie/hci/seniorapp/MenuActivity.java
@@ -0,0 +1,42 @@
+package a1301917.at.ac.univie.hci.seniorapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MenuActivity extends AppCompatActivity {
+    private String[] menuButtonNames;
+    private static final String TAG = "MenuButtonNames";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menue);
+
+        final TextView menuButton1 = (TextView) findViewById(R.id.menuButton1);
+        final TextView menuButton2 = (TextView) findViewById(R.id.menuButton2);
+        final TextView menuButton3 = (TextView) findViewById(R.id.menuButton3);
+        final TextView menuButton4 = (TextView) findViewById(R.id.menuButton4);
+
+        menuButtonNames = getResources().getStringArray(R.array.menue_button_names);
+
+        //Log.i(TAG,menuButtonNames[1].toString());
+
+        menuButton1.setText(menuButtonNames[0]);
+        menuButton2.setText(menuButtonNames[1]);
+        menuButton3.setText(menuButtonNames[2]);
+        menuButton4.setText(menuButtonNames[3]);
+
+    }
+
+    public void MenuOn(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
+    }
+
+    public void MenuOff(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
